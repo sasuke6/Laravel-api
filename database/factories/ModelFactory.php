73b1ca35644e241eb35d6683ca674476cce84aba
Hsu <xusasuke6@gmail.com>
2016-09-05 23:58:57 +0800

    migrate data

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Lesson::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'free' => $faker->boolean()
+    ];
+});
+
